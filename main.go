f65fccf88002ba54668d69af179b9ae7fcc11e53
Black Widow <lua020294@gmail.com>
2024-09-21 09:22:28 -0300

    feat/user: creating user in the database

--- main.go
+++ main.go
@@ -1,42 +1,94 @@
 package main
 
 import (
-    "database/sql"
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"golang.org/x/crypto/bcrypt"
 	_ "github.com/lib/pq"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, MDF!")
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
+var db *sql.DB
+
 func main() {
-    // Pegando as variáveis de ambiente
+	// Pegando as variáveis de ambiente
 	dbHost := os.Getenv("POSTGRES_HOST")
 	dbUser := os.Getenv("POSTGRES_USER")
 	dbPassword := os.Getenv("POSTGRES_PASSWORD")
 	dbName := os.Getenv("POSTGRES_DB")
-    
-    // String de conexão
-    psqlInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", 
-    dbHost, dbUser, dbPassword, dbName)
 
-    // Conectando ao banco de dados
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-    log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
-    }
+	// String de conexão
+	psqlInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", 
+		dbHost, dbUser, dbPassword, dbName)
 
-    err = db.Ping()
-    if err != nil {
-    log.Fatalf("Erro ao pingar o banco de dados: %v", err)
-    }
-    log.Println("Conectado ao banco de dados com sucesso!")
+	// Conectando ao banco de dados
+	var err error
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+	defer db.Close()
 
-    http.HandleFunc("/", helloHandler)
-    http.ListenAndServe(":8080", nil)
+	// Criar a tabela de usuários
+	createUserTable()
+
+	http.HandleFunc("/create-user", createUserHandler)
+
+	log.Println("API rodando na porta 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func createUserTable() {
+	query := `
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			username VARCHAR(50) NOT NULL UNIQUE,
+			password VARCHAR(100) NOT NULL
+		);
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatalf("Erro ao criar a tabela de usuários: %v", err)
+	}
+	log.Println("Tabela de usuários criada ou já existe.")
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Erro ao decodificar o JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Hash da senha
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Erro ao gerar o hash da senha", http.StatusInternalServerError)
+		return
+	}
+
+	// Inserindo o usuário no banco de dados
+	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", user.Username, hashedPassword)
+	if err != nil {
+		http.Error(w, "Erro ao inserir o usuário no banco de dados", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Usuário criado com sucesso"))
 }
